309d0e05a9568cd038c677a4c7448596403411a8
Víctor Hugo Rodriguez Ramos <eskimo@sadzee.com>
2018-11-28 00:50:01 -0600

    WiFi, and directions lights

--- app.js
+++ app.js
@@ -1,66 +1,123 @@
-$(document).ready(function(){
-    window.alert('hello');
+var dire_izquierda = false;
+var dire_derecha = false; 
+var preventivas = false; 
+var faros = false;
+
+$(document).ready(function(){ 
     boot();
 });
 
 function boot(){
-    $('#devices-button').click(function(){
-        $('#devicesList').empty();
-        $('#devicesModal').modal('show');
-        list_devices();
 
-    });
+    var url = "https://api.particle.io/v1/devices/340045000d47363330353437/led?access_token=7eef0bfa715ce44e067d9d9601124210fd3354a3"
+    
 
     $('#del').click(function(){
-        bluetoothSerial.write('6;');
+        var engine = {state : "adelante"}
+        send_data(engine, function(data){
+
+        });
     });
 
     $('#izq').click(function(){
-        bluetoothSerial.write('3;');
+        var engine = {state : "izquierda"}
+        send_data(engine, function(data) {
+            
+        });
     });
 
     $('#det').click(function(){
-        bluetoothSerial.write('7;');
+        var engine = {state : "detener"}
+        send_data(engine, function(data) {
+            
+        });
     });
 
     $('#der').click(function(){
-        bluetoothSerial.write('4;');
+        var engine = {state : "derecha"}
+        send_data(engine, function(data) {
+            
+        });
     });
 
     $('#rev').click(function(){
-        bluetoothSerial.write('5;');
-    });
-}
+        var engine = {state : "reversa"}
+        send_data(engine, function(data) {
+            
+        });
+    }); 
 
-function list_devices(){
-    bluetoothSerial.list(function(devices){
+    $('#faros').click(function(){
+        var light;
+        if(faros){
+            faros = false;
+            var light = {state: "faros_off"}
+        }else{
+            var light = {state: "faros_on"}
+            faros = true;
+        }
 
-        $.each(devices, function(index, value){
-
-            var item =
-             $('<button></button>').attr("class", "list-group-item")
-                .attr("type", "button")
-                .text(value.name)
-                .click(function(){
-
-                 bluetoothSerial.connect(value.id, 
-                 function(){
-                     window.alert('Dispositivo conectado');
-                 }, 
-                 function(){
-                     window.alert('Dispositivo desconectado');
-                 });
-             })
-
-             $('#devicesList').append(item);
-
-            //window.alert(JSON.stringify(value));
+        send_data(light, function(){
 
         });
+    })
 
-    }, 
+    $('#dire_izquierda').click(function(){
+        var light;
+        if(dire_izquierda){
+            dire_izquierda = false;
+            var light = {state: "dire_izquierda_off"}
+        }else{
+            var light = {state: "dire_izquierda_on"}
+            dire_izquierda = true;
+        }
 
-    function(){
-        window.alert("algo falló :(");
-    });
+        send_data(light, function(){
+
+        });
+    }) 
+
+    $('#dire_derecha').click(function(){
+        var light;
+        if(dire_derecha){
+            dire_derecha = false;
+            light = {state: "dire_derecha_off"}
+        }else{
+            light = {state: "dire_derecha_on"}
+            dire_derecha = true;
+        }
+
+        send_data(light, function(){
+
+        });
+    }) 
+
+    $('#preventivas').click(function(){
+        var light;
+        if(preventivas){
+            preventivas = false;
+            light = {state: "preventivas_off"}
+        }else{
+            light = {state: "preventivas_on"}
+            preventivas = true;
+        }
+
+        send_data(light, function(){
+
+        });
+    })
+
+
 }
+
+function send_data(data, result) {
+    var url = "https://api.particle.io/v1/devices/3e003d000247343339373536/engine?access_token=7eef0bfa715ce44e067d9d9601124210fd3354a3";
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        success: result,
+        dataType: "application/json"
+      });
+}
+
